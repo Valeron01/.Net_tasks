7f8f2e1ad7819caa2b2e7deb3e1a735c27e9c31d
Valeron01 <43704486+Valeron01@users.noreply.github.com>
2021-11-06 17:08:07 +0300

    Add files via upload

---
+++ Task3-Window/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3_Window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "";
+
+            Random r = new Random();
+            Van van = new Van();
+
+            float totalPrice = 10000;
+
+            while (van.calcPrice() < totalPrice)
+            {
+                int coffeeType = r.Next(0, 3);
+
+                switch (coffeeType)
+                {
+                    case 0:
+                        van.addCoffee(new BlackCardCoffee());
+                        break;
+                    case 1:
+                        van.addCoffee(new RoundJarCoffe());
+                        break;
+                    case 2:
+                        van.addCoffee(new SmallPacketCoffee());
+                        break;
+                }
+            }
+
+            foreach (var coffe in van.getBag())
+            {
+                richTextBox1.Text += coffe.ToString() + "\n";
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Task3-Window/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task3_Window
+{
+    class Coffee
+    {
+        public readonly float weight;
+
+        public readonly float width;
+        public readonly float height;
+        public readonly float length;
+
+        public readonly float price;
+
+        public Coffee(float weight, float width, float height, float length, float price)
+        {
+            this.weight = weight;
+            this.width = width;
+            this.height = height;
+            this.length = length;
+            this.price = price;
+        }
+
+        public float Volume
+        {
+            get { return width * height * length; }
+        }
+
+        public override string ToString()
+        {
+            return $"Base coffee, {width}x{length}x{height}, {Volume} cm2, {price} rub";
+        }
+    }
+
+    class BlackCardCoffee : Coffee
+    {
+        public BlackCardCoffee() : base(150, 5, 10, 5, 150)
+        {
+
+        }
+        public override string ToString()
+        {
+            return $"Black Card Coffee, {width}x{length}x{height}, {Volume} cm2, {price} rub";
+        }
+
+    }
+
+    class RoundJarCoffe : Coffee
+    {
+        public RoundJarCoffe() : base(300, 7, 15, 7, 300)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Round Jar Coffe, {width}x{length}x{height}, {Volume} cm2, {price} rub";
+        }
+    }
+
+    class SmallPacketCoffee : Coffee
+    {
+        public SmallPacketCoffee() : base(3, 2, 10, 2, 30)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"Small Packet Coffee, {width}x{length}x{height}, {Volume} cm2, {price} rub";
+        }
+    }
+
+
+    class Van
+    {
+        private readonly List<Coffee> bag = new List<Coffee>();
+
+        public void addCoffee(Coffee c)
+        {
+            bag.Add(c);
+        }
+
+        public float calcWeight()
+        {
+            float result = 0;
+
+            foreach (var item in bag)
+            {
+                result += item.weight;
+            }
+
+            return result;
+        }
+
+        public float calcPrice()
+        {
+            float result = 0;
+
+            foreach (var item in bag)
+            {
+                result += item.price;
+            }
+
+            return result;
+        }
+
+        public float calcVolume()
+        {
+            float result = 0;
+
+            foreach (var item in bag)
+            {
+                result += item.Volume;
+            }
+
+            return result;
+        }
+
+        public ReadOnlyCollection<Coffee> getBag()
+        {
+            return new ReadOnlyCollection<Coffee>(bag);
+        }
+    }
+
+
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
