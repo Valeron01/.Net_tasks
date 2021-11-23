73b7bab9352fbb19f8ace3389a3df71f4f17ccc9
Valery Zaporozhetc <43704486+Valeron01@users.noreply.github.com>
2021-11-23 09:18:21 +0300

    Added 4 Task

---
+++ Task4/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace Task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.NameText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IncomeText = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RatingText = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.InvestmentText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "BaseClass",
+            "ChildClass"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(145, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // NameText
+            // 
+            this.NameText.Location = new System.Drawing.Point(57, 46);
+            this.NameText.Name = "NameText";
+            this.NameText.Size = new System.Drawing.Size(100, 20);
+            this.NameText.TabIndex = 2;
+            this.NameText.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Income";
+            // 
+            // IncomeText
+            // 
+            this.IncomeText.Location = new System.Drawing.Point(57, 72);
+            this.IncomeText.Name = "IncomeText";
+            this.IncomeText.Size = new System.Drawing.Size(100, 20);
+            this.IncomeText.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Rating";
+            // 
+            // RatingText
+            // 
+            this.RatingText.Location = new System.Drawing.Point(57, 98);
+            this.RatingText.Name = "RatingText";
+            this.RatingText.Size = new System.Drawing.Size(100, 20);
+            this.RatingText.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Investment";
+            // 
+            // InvestmentText
+            // 
+            this.InvestmentText.Location = new System.Drawing.Point(74, 124);
+            this.InvestmentText.Name = "InvestmentText";
+            this.InvestmentText.Size = new System.Drawing.Size(83, 20);
+            this.InvestmentText.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Save&&Print";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(273, 169);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.InvestmentText);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.RatingText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.IncomeText);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.NameText);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox NameText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IncomeText;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox RatingText;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox InvestmentText;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Task4/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task4
+{
+    public partial class Form1 : Form
+    {
+        class BaseClass
+        {
+            protected readonly float income;
+            protected readonly float rating;
+            protected readonly string name;
+
+            public BaseClass(string name, float income, float rating)
+            {
+                this.name = name;
+                this.income = income;
+                this.rating = rating;
+            }
+
+            public virtual float Quality()
+            {
+                return income * rating;
+            }
+
+            public override string ToString()
+            {
+                return "Base class of company " + name + " with Quality " + Quality();
+            }
+        }
+
+
+        class ChildClass : BaseClass
+        {
+            float investment;
+            public ChildClass(string name, float income, float rating, float investment) : base(name, income, rating)
+            {
+                this.investment = investment;
+            }
+
+            public override float Quality()
+            {
+                return (float)Math.Pow(investment, 3) + base.Quality();
+            }
+
+            public override string ToString()
+            {
+                return "Child class of company " + name + " with Quality " + Quality();
+            }
+        }
+
+        private BaseClass baseClass;
+        private ChildClass childClass;
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ComboBox comboBox = (ComboBox)sender;
+            if (comboBox.SelectedIndex == 0)
+            {
+                InvestmentText.Enabled = false;
+                label4.Enabled = false;
+            }
+            else
+            {
+                InvestmentText.Enabled = true;
+                label4.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bool isBase = comboBox1.SelectedIndex == 0;
+
+            string name = NameText.Text;
+            float income = 0;
+            float rating = 0;
+            float investment = 0;
+            try
+            {
+                income = float.Parse(IncomeText.Text);
+                rating = float.Parse(RatingText.Text);
+                if(!isBase)
+                    investment = float.Parse(InvestmentText.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Проверьте правильность введённых данных!");
+            }
+
+            if (isBase)
+            {
+                baseClass = new BaseClass(name, income, rating);
+                MessageBox.Show(baseClass.ToString());
+            }
+
+            if (!isBase)
+            {
+                childClass = new ChildClass(name, income, rating, investment);
+                MessageBox.Show(childClass.ToString());
+            }
+        }
+    }
+}
